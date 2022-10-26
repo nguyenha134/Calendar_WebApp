3b986d2cad3a08ee5a1fdba2d541b98943ff678f
nguyenha134 <nguyenhoangha1342001@gmail.com>
2022-10-26 23:18:11 +0700

    "Update"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", './public/index.html',
-  ],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    //Because we made a dynamic class with the label we need to add those clases
+    // to the safe list so the purge does not remove that
+    safelist: [
+      ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+      ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+      ...labelsClasses.map((lbl) => `text-${lbl}-400`)
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["Open Sans"]
       },
-      gridTemplateColums: {
+      gridTemplateColumns: {
         "1/5": "1fr 5fr"
       }
     },
@@ -15,7 +32,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require("@tailwindcss/forms")],
 }
